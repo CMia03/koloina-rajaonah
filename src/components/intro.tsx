57e2158ff85f64018a77927e9006abbf0ce1a5c9
Cathynuklear <cathy.rakotoarizao@nuklear.io>
2025-04-01 11:23:46 +0300

    delete github

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -6,7 +6,6 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
-import { FaGithubSquare } from "react-icons/fa";
 import { HiDownload } from "react-icons/hi";
 
 export default function Intro() {
@@ -103,14 +102,6 @@ export default function Intro() {
           target="_blank"
         >
           <BsLinkedin />
-        </a>
-
-        <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
-          target="_blank"
-        >
-          <FaGithubSquare />
         </a>
       </motion.div>
     </section>
